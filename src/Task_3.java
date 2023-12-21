481045fa0378bc454228762f7643a1054a475f2d
Arraht <igor_161995@mail.ru>
2023-12-21 19:42:13 +0600

    fix cast to integer

--- src/Task_3.java
+++ src/Task_3.java
@@ -2,7 +2,7 @@ public class Task_3 {
     public static void main(String[] args) {
         int population = 12_000_000;
         float birthrate = 17 * 0.001f, deathRate = 8 * 0.001f;
-        float populationIncrease = Math.round(population * birthrate), populationDecline = Math.round(population *
+        int populationIncrease = (int)(population * birthrate), populationDecline = (int)(population *
                 deathRate);
         for (int i = 1; i <= 10 ; i++) {
             population += (populationIncrease - populationDecline);
